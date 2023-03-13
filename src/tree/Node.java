0b05ac6eb95e885467faf7c315f2508a09cc2f0e
Artur <artur.gikramin@yandex.ru>
2023-03-13 22:12:30 +0500

    finalComit

---
+++ src/tree/Node.java
@@ -0,0 +1,10 @@
+//package tree;
+//
+//
+//public class Node<V extends Comparable<V>> {
+//    protected V value;
+//
+//    protected Color color;
+//    protected Node left;
+//    protected Node right;
+//}
---
+++ src/tree/Tree.java
@@ -0,0 +1,108 @@
+package tree;
+
+public class Tree<V extends Comparable<V>> {
+    Node root;
+
+    public boolean add(V value) {
+        if (root != null) {
+            boolean result = addNode(root, value);
+            root = rebalanced(root);
+            root.color = Color.BLACK;
+            return result;
+        } else {
+            root = new Node();
+            root.color = Color.BLACK;
+            root.value = value;
+            return true;
+        }
+    }
+
+    private boolean addNode(Node node, V value) {
+
+        if (node.value == value) {
+            return false;
+        } else {
+            if (node.value.compareTo(value) > 0) {
+                if (node.left != null) {
+                    boolean result = addNode(node.left, value);
+                    node.left = rebalanced(node.left);
+                    return result;
+                } else {
+                    node.left = new Node();
+                    node.left.color = Color.RED;
+                    node.left.value = value;
+                    return true;
+                }
+            } else {
+                if (node.right != null) {
+                    boolean result = addNode(node.right, value);
+                    node.right = rebalanced(node.right);
+                    return result;
+                } else {
+                    node.right = new Node();
+                    node.right.color = Color.RED;
+                    node.right.value = value;
+                    return true;
+                }
+            }
+        }
+    }
+
+    private Node rebalanced(Node node) {
+        Node result = node;
+        boolean needRebalanced;
+        do {
+            needRebalanced = false;
+            if (result.right != null && result.right.color == Color.RED &&
+                    (result.left == null || result.left.color == Color.BLACK)) {
+                needRebalanced = true;
+                result = rightSwap(result);
+            }
+            if (result.left != null && result.left.color == Color.RED &&
+                    result.left.left != null && result.left.left.color == Color.RED) {
+                needRebalanced = true;
+                result = leftSwap(result);
+            }
+            if (result.left != null && result.left.color == Color.RED &&
+                    result.right != null && result.right.color == Color.RED) {
+                needRebalanced = true;
+                colorsSwap(result);
+            }
+        }
+        while (needRebalanced);
+        return result;
+    }
+
+    private Node rightSwap(Node node) {
+        Node rightChild = node.right;
+        Node betweenChild = rightChild.left;
+        rightChild.left = node;
+        node.right = betweenChild;
+        rightChild.color = node.color;
+        node.color = Color.RED;
+        return rightChild;
+    }
+
+    private Node leftSwap(Node node) {
+        Node leftChild = node.left;
+        Node betweenChild = leftChild.right;
+        leftChild.right = node;
+        node.left = betweenChild;
+        leftChild.color = node.color;
+        node.color = Color.RED;
+        return leftChild;
+    }
+
+    private void colorsSwap(Node node) {
+        node.right.color = Color.BLACK;
+        node.left.color = Color.BLACK;
+        node.color = Color.RED;
+    }
+    public class Node {
+        public V value;
+
+        public Color color;
+        public Node left;
+        public Node right;
+    }
+}
